5fb8f7f51fe6398476ff4ffde8675f3ad6a4f070
Jamie T <87329840+jamietaube@users.noreply.github.com>
2022-07-26 13:03:39 -0700

    Added player camera

--- TCG22/Assets/Scripts/FollowPlayer.cs
+++ TCG22/Assets/Scripts/FollowPlayer.cs
@@ -14,7 +14,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 4, -10);
+    private Vector3 offset = new Vector3(0.05f, 0.05f, -10);
 
     // Start is called before the first frame update
     void Start()
