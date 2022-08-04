aa7051f137909b87a2bb580b870c8aecaed551d7
Priyansh Nayak <priyansh.nayak.28@gmail.com>
2022-08-04 18:12:03 +0530

    Added a player manager to swap between players after each turn
    Currently needs to press S during attack phase to swap

--- TCG22/Assets/Scripts/FollowPlayer.cs
+++ TCG22/Assets/Scripts/FollowPlayer.cs
@@ -16,10 +16,9 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0.05f, 0.05f, -10);
 
-    // Start is called before the first frame update
-    void Start()
+    public void setPlayer(GameObject newPlayer)
     {
-
+        player = newPlayer;
     }
 
     // Update is called once per frame
