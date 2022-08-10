1c9cacce5185bfc821ca6b4a5b882533ccdf5f57
Jamie T <87329840+jamietaube@users.noreply.github.com>
2022-08-10 15:28:57 -0700

    Modify BGRepeater.cs to oscillate the background smoothly

--- TCG22/Assets/Scripts/Menus/BGRepeater.cs
+++ TCG22/Assets/Scripts/Menus/BGRepeater.cs
@@ -7,22 +7,26 @@ public class BGRepeater : MonoBehaviour
     private float speed;
     public Vector3 startPos;
     public float repeatWidth;
-    public float x;
+    public float xMin;
 
     void Start()
     {
         speed = 5f;
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
-        x = startPos.x - repeatWidth;
+        //repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        repeatWidth = GetComponent<BoxCollider>().size.x;
+        xMin = startPos.x - repeatWidth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - repeatWidth)
+        if (transform.position.x < xMin || transform.position.x > startPos.x)
         {
-            transform.position = startPos;
+            //transform.position = startPos;
+
+            // reverse direction
+            speed *= -1;
         }
         transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
     }
